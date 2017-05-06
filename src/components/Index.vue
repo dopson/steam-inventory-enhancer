be54168d718aa3638289c8031d44b2cdec8156df
Dopson <vili.tielinen@outlook.com>
2017-05-06 10:43:22 +0300

    Initial structure

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Index component</h1>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
